a998256def4dac8d110a41cf03e4254053277a53
Rodrigo Arcova <rarcova@CLOSEUPBRZ.COM.BR>
2019-11-26 17:34:24 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from "react-redux"
 import store from './store'
-import Cursos from './components/Cursos'
+import Cursos from './components/cursos'
 function App() {
   return (
     <Provider store={store}>
