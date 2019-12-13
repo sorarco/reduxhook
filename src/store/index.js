b26963f2ba7d18134d76d31c289b1c9fe302961d
Rodrigo Arcova <rodrigoarcova@Rodrigos-MacBook-Pro.local>
2019-12-13 14:46:01 -0300

    redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import history from "../routes/history";
 import reducers from "../reducers";
@@ -8,8 +9,8 @@ import reducers from "../reducers";
 const middlewares = [routerMiddleware(history), thunk];
 
 const store = createStore(
-    connectRouter(history)(reducers),
-    applyMiddleware(...middlewares)
+  connectRouter(history)(reducers),
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 export default store;
